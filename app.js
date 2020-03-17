f467cfb5693bf50d84e771a48927e10601b5c1da
Ceejaymar <martinezcj2@gmail.com>
2020-03-16 20:15:54 -0400

    Create attack function

--- app.js
+++ app.js
@@ -12,7 +12,30 @@ new Vue({
       this.monsterHealth = 100;
     },
     attack: function() {
+      let max = 10;
+      let min = 3;
+      let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
 
+      this.monsterHealth -= damage;
+
+      if (this.monsterHealth <= 0) {
+        alert('You beat the monster!');
+        this.gameIsRunning = false;
+        return;
+      }
+
+      max = 12;
+      min = 5;
+
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+
+      this.playerHealth -= damage;
+
+      if (this.playerHealth <= 0) {
+        alert('The monster got you!');
+        this.gameIsRunning = false;
+        return;
+      }
     },
     specialAttack: function() {
 
